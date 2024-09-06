105a945236dab68d72dfcbc5d3246d39345fca3d
SURAJ YADAV <55278919+S-yit@users.noreply.github.com>
2024-09-06 16:24:58 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log(3 + 3); 
+//code readability should be high
+console.log("Suraj");
+
+let name = "Suraj"
+let age = 23
+let isLoggedIn = false
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//undefined => value not assigned
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined); //undifined
+console.log(typeof null); //object
