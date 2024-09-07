7fc3b84173c1bd1ee1e7346dbafa31e98828849a
SURAJ YADAV <55278919+S-yit@users.noreply.github.com>
2024-09-07 10:53:52 +0000

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,24 @@ const myFunction = function(){
 
 console.log(typeof bigNumber);
 console.log(typeof myFunction);
+
+//-------------------------------------------------------------------------------
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename = "surajyadavdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let user1 = {
+    email:"user@gamil.com",
+    upi:"upi@ybl"
+}
+
+let user2 = user1
+
+user2.email = "suraj@gmail.com"
+console.log(user1.email)
+console.log(user2.email);
